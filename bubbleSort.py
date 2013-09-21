704f2fcce977a59b9281cb132e1ca09cbfe23129
M42 <mikrokosmos42@gmail.com>
2013-09-21 13:07:23 +0200

    BubbleSort: O(n) i worst case.

--- bubbleSort.py
+++ bubbleSort.py
@@ -6,12 +6,18 @@ def bubbleSort(data):
     plotting = sorting.Plotter(data)
     plotting.snapshot()
 
-    for i in range(len(data)-1):
+    is_sorted = False
+    i = 0
+
+    while not is_sorted:
+        is_sorted = True
+
         for j in range(len(data)-1-i):
             if data[j] > data[j+1]:
                 data[j],data[j+1] = data[j+1],data[j]
+                is_sorted = False
+        i = i+1
 
         plotting.snapshot()
     plotting.end()
-    
     return data
