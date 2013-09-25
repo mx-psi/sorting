fb24db54ff2fb01c6bbf82d951e296fb07bebe06
Psi <pbaeyens31@gmail.com>
2013-09-25 21:27:51 +0200

    Rewritten for new structure.

--- oddEvenSort.py
+++ oddEvenSort.py
@@ -1,35 +1,25 @@
 # oddEvenSort.py
 
-##
-# The snapshots are made outside the loops for making videos shorter. 
-#
-# If you want a step-by-step video, place them inside the for loops
-##
-
 import sorting
 
 def oddEven(data, plot = True):
     """ Odd-even sort. """
-    plotting = sorting.Plotter(data, plot)
-    plotting.snapshot()
+    data = sorting.start()
     n = len(data) -1
     is_sorted = False
+    a = 0
     while not is_sorted:
+        
         is_sorted = True
-        for i in range(0, n, 2):
-            #Compares each couple [even,odd]
+        
+        for i in range(a, n, 2):
             if data[i] > data[i+1]:
-                #Swaps and sets is_sorted to False
-                data[i],data[i+1] = data[i+1],data[i]
+                sorting.swap(i,i+1)
                 is_sorted = False
-        plotting.snapshot()
-        for i in range(1, n, 2):
-            #Compares each couple [odd,even]
-            if data[i] > data[i+1]:
-                #Swaps and sets is_sorted to False
-                data[i],data[i+1] = data[i+1],data[i]
-                is_sorted = False
-        plotting.snapshot()
-    plotting.end()
+                
+        a = not a 
+        sorting.iteration()
+        
+    sorting.end()
     return data
         
