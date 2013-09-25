2cedcf416bd1a35be60109ff3fc83ce6ea980887
Psi <pbaeyens31@gmail.com>
2013-09-25 23:21:43 +0200

    Added support for representing swapped points

--- sorting.py
+++ sorting.py
@@ -72,6 +72,7 @@ def start():
 def swap(i,j):
     """ Swaps the contents of data[i] and data[j]. """
     data[i],data[j] = data[j],data[i]
+    if plot_swaps: plotting.swaps(i,j)
 
 def iteration():
     """ It is called when the most external loop finishes the current iteration. """
