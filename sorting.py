b651a85082146d26a26fd3b942c4738e1eaeae42
Psi <pbaeyens31@gmail.com>
2013-09-26 17:01:54 +0200

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -74,7 +74,8 @@ def start():
 def swap(i,j):
     """ Swaps the contents of data[i] and data[j]. """
     data[i],data[j] = data[j],data[i]
-    if plot_swaps: plotting.swaps(i,j)
+    # Uncomment line below when plotting.swaps works correctly.
+    # if plot_swaps: plotting.swaps(i,j)
 
 def iteration():
     """ It is called when the most external loop finishes the current iteration. """
