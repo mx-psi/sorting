8795d35b093f51bb84ae6d066575e850fac7523a
Psi <pbaeyens31@gmail.com>
2013-09-28 12:50:00 +0200

    Updated info

--- make.py
+++ make.py
@@ -1,7 +1,7 @@
 ##
 # Sorting algorithms visualized.
 #
-# This code uses python2, pylab and ffmpeg. You may need to install them.
+# This code uses python2, matplotlib and avconv. You may need to install them.
 #
 # The idea was taken from "The Glowing Python":
 #    http://glowingpython.blogspot.com.es/2013/02/selection-sort-animated.html
@@ -9,7 +9,7 @@
 
 # make.py
 # This file contains an example of use.
-# It will create an animation of BubbleSort.
+# It will create an animation of the algorithm in sorting.algorithm.
 
 import sorting
 import bubbleSort
